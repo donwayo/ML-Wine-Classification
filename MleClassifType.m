dc807b978ac556acea537ad186db41c330939661
irene <paspatoufi@gmail.com>
2014-10-10 19:33:08 +0300

    fixed mle_type functions

--- MleClassifType.m
+++ MleClassifType.m
@@ -9,6 +9,7 @@ wine = W(randperm(height(W)),:);
 train_set = wine(1:n_train,:);
 valid_set = wine(n_train+1:end, :);
 
+feat = 2;
 r = W(1:n_train,13);
 x = wine{1:n_train, 1:11};
 
@@ -23,21 +24,5 @@ r_white = ones(1,height(whites));  % if wine is white, give value 1
 r_bl = [r_red r_white];
 
 mle_pr = sum(r_bl) / n_train
-% mle_p_red = (a + sum(r_red*x_red)) / (K*a + sum(r_red))
-feat = 2;
-mle_p_white = sum(r_white*x_white(:,feat))/ sum(r_white);
-for i=1:11
-    mle_p_red(i) = (a + sum(r_red*x_red(:,i))) / (K*a + sum(r_red));
-    mle_p_white(i) = sum(r_white*x_white(:,i))/ sum(r_white);
-end
-mle_p_red, mle_p_white
-% mle_p_red = sum( r_red*x_red ) / sum(r_red)
-% mle_p_white = (a + sum(r_white*x_white))/ (K*a + sum(r_white))
-% mle_p_white = sum( r_white*x_white ) / sum(r_white)
-% mle_p_red = height(reds)/n_train;
-% mle_p_white = height(whites)/n_train;
-
-% w_0 = sum( log(1-mle_p_0j) ) + log(1-mle_pr) - sum( log(1-mle_p_1j) ) - log(mle_pr) 
-%     % w_0 =   -2.4689 - 3.1416i
-% w = sum( log( mle_p_0j*(1-mle_p1j) ) ) - sum( log( mle_p_1j*(1-mle_p0j) ) )
-%     % w =    1.0447e+02 + 1.1938e+02i
+mle_p_red = sum(x_red(:,feat)) / length(r_red)
+mle_p_white = sum(x_white(:,feat)) / length(r_white)
